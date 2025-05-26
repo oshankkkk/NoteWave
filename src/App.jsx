db57a69fd3dd912c6ddcc9271078231fe1c474fb
Oshan <oshankodagoda200667@gmail.com>
2025-05-26 13:15:10 +0530

    add email/password and Google authentication firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import LoginPage from "./Login"
+
 function App(){
 return(
-<div className="flex justify-center min-h-screen items-center">
-  <h1>NoteWave</h1>
-</div>
+  <LoginPage></LoginPage>
   
 )
 }
