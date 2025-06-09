2a9d1341e9aa7a9e883634cbb9062db87153c641
Oshan <oshankodagoda200667@gmail.com>
2025-06-09 14:23:36 +0530

    add main code to app jsx for ease access for me

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import Calendar from './Calendar'; // Make sure the path is correct
-
-// function App() {
-//     return (
-//         <div>
-//             <Calendar />
-//         </div>
-//     );
-// }
-
-// export default App;
-
-// import LoginPage from "./LoginPage"
-// import SignUpPage from "./SignUpPage"
-// function App(){
-
-// return(
-// <SignUpPage></SignUpPage>
-// )
 
 // import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import Home from "./Home";
@@ -30,6 +10,48 @@
 // import './index.css';
 // import NavBar from './Layout.jsx';
 // import SignUpPage from './Authentication/SignUpPage.jsx';
+// import LoginPage from './Authentication/LoginPage.jsx';
+
+// function App(){
+// const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [loading, setLoading] = useState(true); 
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       setIsLoggedIn(!!user);
+//       setLoading(false);
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   return (
+//     // <div>
+//     //   <Sidebar />
+//     //   <Groups />
+//     // </div>
+//     <BrowserRouter>
+//       <Routes>
+//         {/* Protected routes under AppLayout */}
+//         <Route
+//           path="/"
+//           element={isLoggedIn ? <NavBar /> : <Navigate to="/login" replace />}
+//         >
+//           <Route index element={<Home />} />
+         
+//         </Route>
+
+//         {/* Public route */}
+//         <Route
+//           path="/login"
+//           element={!isLoggedIn ? <SignUpPage /> : <Navigate to="/" replace />}
+//         />
+//         <Route path="/signup" element={!isLoggedIn ? <SignUpPage /> : <Navigate to="/" replace />} />
+//         <Route path="/login" element={!isLoggedIn ? <LoginPage/> : <Navigate to="/" replace />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 import LoginPage from './Authentication/LoginPage.jsx';
 
 function App(){
@@ -39,18 +61,3 @@ return(
 )
 }
 export default App;
-
-// import React from "react";
-// import { Sidebar } from "./assets/SideBar/Sidebar";
-// im
-// port Groups from "./Components/Groups";
-
-// function App() {
-//   return (
-//     <div>
-//       <Sidebar />
-//       <Groups />
-//     </div>
-//   );
-// }
-// export default App;
