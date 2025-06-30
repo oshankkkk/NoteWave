5591448c2fe5e1adb9a60483a52edae4d1bb96fa
Oshan <oshankodagoda200667@gmail.com>
2025-06-30 14:33:44 +0530

    intergrate exit grp button

--- src/GroupSideBar/GroupSideBar.jsx
+++ src/GroupSideBar/GroupSideBar.jsx
@@ -3,7 +3,8 @@ import GroupNameAndAvatar from "./GroupNameAvatar";
 import MemberList from "./MemberList";
 import { useState,useEffect } from "react";
 import { db } from "../firebase-config";
-export default function GroupSideBar({groupData}){
+import { ExitGroup } from "./ExitGroup";
+export default function GroupSideBar({groupData,userId,setActiveSideBar}){
 const [groupInfo, setGroupInfo] = useState(null);
 
 // useEffect(() => {
@@ -32,15 +33,16 @@ const [groupInfo, setGroupInfo] = useState(null);
 <div className=" inset-y-0 right-0  w-1/4 absolute">
       <div className="bg-green-600 text-white p-4 flex items-center justify-between">
         <h2 className="text-lg font-medium">Group info</h2>
-        <span className="text-2xl cursor-pointer hover:bg-green-700 rounded p-1">×</span>
+
+        {/* <button className="text-2xl cursor-pointer hover:bg-green-700 rounded p-1">×</button> */}
+        <button onClick={setActiveSideBar(false)} className="text-2xl cursor-pointer hover:bg-green-700 rounded p-1">×</button>
       </div>
-      {console.log("eheh")}
  <div className="flex-1 overflow-y-auto">
 
-<GroupNameAndAvatar groupData={groupData} adminId={groupData.Admin} name={groupData.Name}></GroupNameAndAvatar>
-<GroupDescription description={groupData.Description}></GroupDescription>
-<MemberList members={groupData.Member}></MemberList>
-
+<GroupNameAndAvatar groupMembers={groupData.Member} adminId={groupData.Admin} name={groupData.Name} groupId={groupData.id}></GroupNameAndAvatar>
+<GroupDescription description={groupData.Description} groupId={groupData.id}></GroupDescription>
+<MemberList members={groupData.Member} adminId={groupData.Admin}></MemberList>
+<ExitGroup groupId={groupData.id} userId={userId} ></ExitGroup>
  </div>
 </div>
     )
